a6765be7e900a893b284afc5afd05cf704683f4a
xyrlan <lobatopedro68@gmail.com>
2023-08-19 08:58:21 -0400

    node converter script

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
-const { spawn } = require("child_process");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async afterBuild() {
-        console.log("Executando o script de conversão...");
-        const childProcess = spawn("node", ["dist/converter/index.js"]);
-
-        childProcess.stdout.on("data", (data) => {
-            console.log(`Saída do script: ${data}`);
-        });
-
-        childProcess.stderr.on("data", (data) => {
-            console.error(`Erro no script: ${data}`);
-        });
-
-        childProcess.on("close", (code) => {
-            console.log(`O script foi executado com código de saída: ${code}`);
-        });
-    },
+    scripts: {
+        postbuild: "npm run convert-data"
+    }
 }
 
 module.exports = nextConfig
