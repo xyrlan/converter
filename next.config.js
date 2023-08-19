ff531aae3190c9f4a513be434b79438e9e50c9a4
xyrlan <lobatopedro68@gmail.com>
2023-08-19 08:49:09 -0400

    fix run converter script

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+const { spawn } = require("child_process");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async afterBuild() {
+        console.log("Executando o script de conversão...");
+        const childProcess = spawn("node", ["dist/converter/index.js"]);
+
+        childProcess.stdout.on("data", (data) => {
+            console.log(`Saída do script: ${data}`);
+        });
+
+        childProcess.stderr.on("data", (data) => {
+            console.error(`Erro no script: ${data}`);
+        });
+
+        childProcess.on("close", (code) => {
+            console.log(`O script foi executado com código de saída: ${code}`);
+        });
+    },
+}
 
 module.exports = nextConfig
